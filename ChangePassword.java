a21771dd4eca7a53c5b2a5fcc4f6ad2761b43ee6
Smit Mehta1 <smit.mehta1@ibm.com>
2022-07-20 11:09:25 +0530

    created ForgotPassword.java and ChangePassword.java

---
+++ ChangePassword.java
@@ -0,0 +1,9 @@
+public class ChangePassword
+{
+	public static void main(String args[])
+	{
+		
+		System.out.println("From Change password ");
+	}
+
+}
---
+++ ForgotPassword.java
@@ -0,0 +1,9 @@
+public class ForgotPassword
+{
+	public static void main(String args[])
+	{
+		
+		System.out.println("From forgot password");
+	}
+
+}
